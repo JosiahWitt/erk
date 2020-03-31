711ea366b44b49dad1955813aa749de8e7dc3bb4
Josiah Witt <wittj@nextcapital.com>
2020-03-31 19:00:58 -0400

    Add erkjson package

---
+++ erkjson/erkjson.go
@@ -0,0 +1,111 @@
+// Package erkjson allows exporting errors as JSON.
+//
+// To use this, first embed JSONWrapper in your project's default error kind.
+// Then switch embedding erk.DefaultKind to erk.DefaultPtrKind, to enforce all kinds to be pointers.
+//
+// Example:
+//  // Declare a default kind for your project:
+//  type DefaultKind struct {
+//    erk.DefaultPtrKind
+//    erkjson.JSONWrapper
+//  }
+//
+//  // Declare your kinds:
+//  type ErkNotFound struct { DefaultKind }
+//
+//  // Create your errors:
+//  var ErrSingleItemNotFound = erk.New(&ErkNotFound{}, "a single item with key '{{.key}}' was not found")
+//
+//  // Export your errors to JSON:
+//  jsonError := erkjson.ExportError(err)
+//  // jsonError is an error of type *ErkNotFound, and jsonError.Error() returns the JSON of the original error.
+//
+//  // The original error can be obtained with:
+//  errors.Unwrap(jsonError)
+//
+// It is not required to embed the JSONWrapper, or use pointers to kinds.
+// However, you will lose the kind as the return type.
+package erkjson
+
+import (
+	"encoding/json"
+
+	"github.com/JosiahWitt/erk"
+)
+
+// JSONWrapable must be implemented for ExportError to export the error as JSON with the kind's type.
+type JSONWrapable interface {
+	error
+	SetJSONError(jsonError string)
+	SetOriginalError(originalError error)
+	IsNil() bool
+}
+
+// JSONWrapper can be embedded in error kinds.
+// It is recommended to embed it in your project's default kind.
+type JSONWrapper struct {
+	jsonError     string
+	originalError error
+}
+
+// JSONWrapper implements JSONWrapable.
+var _ JSONWrapable = &JSONWrapper{}
+
+// SetJSONError string, which is returned by Error().
+func (w *JSONWrapper) SetJSONError(jsonError string) {
+	w.jsonError = jsonError
+}
+
+// Error returns the JSON string.
+func (w *JSONWrapper) Error() string {
+	return w.jsonError
+}
+
+// SetOriginalError returned by Unwrap().
+func (w *JSONWrapper) SetOriginalError(originalError error) {
+	w.originalError = originalError
+}
+
+// Unwrap returns the original error.
+func (w *JSONWrapper) Unwrap() error {
+	return w.originalError
+}
+
+// IsNil returns true if the JSONWrapper is nil.
+func (w *JSONWrapper) IsNil() bool {
+	return w == nil
+}
+
+// ExportError to JSON.
+//
+// If the error's kind implements the JSONWrapable interface, the JSON error is set on the kind.
+// Otherwise, the JSON error is returned as a JSONWrapper.
+func ExportError(originalError error) error {
+	erkErr := erk.ToErk(originalError)
+
+	jsonError, err := json.Marshal(erkErr)
+	if err != nil {
+		jsonError, _ = json.Marshal(erk.BaseExport{
+			Kind:    "erk:error_is_invalid_json",
+			Message: "The error cannot be wrapped as JSON: " + err.Error(),
+			Params: erk.Params{
+				"err": erkErr.Error(),
+			},
+		})
+	}
+
+	wrapper := getJSONWrapper(erkErr)
+	wrapper.SetJSONError(string(jsonError))
+	wrapper.SetOriginalError(originalError)
+	return wrapper
+}
+
+func getJSONWrapper(err erk.Kindable) JSONWrapable {
+	kind := err.Kind()
+	wrapable, ok := kind.(JSONWrapable)
+	if ok && !wrapable.IsNil() {
+		return wrapable
+	}
+
+	return &JSONWrapper{}
+}
---
+++ erkjson/erkjson_test.go
@@ -0,0 +1,177 @@
+package erkjson_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/JosiahWitt/erk"
+	"github.com/JosiahWitt/erk/erkjson"
+	"github.com/matryer/is"
+)
+
+func TestSetJSONError(t *testing.T) {
+	is := is.New(t)
+	const jsonError = `{"this": "is", "json": "!"}`
+
+	wrapper := &erkjson.JSONWrapper{}
+	wrapper.SetJSONError(jsonError)
+	is.Equal(wrapper.Error(), jsonError)
+}
+
+func TestSetOriginalError(t *testing.T) {
+	is := is.New(t)
+	var originalError = errors.New("something bad happened")
+
+	wrapper := &erkjson.JSONWrapper{}
+	wrapper.SetOriginalError(originalError)
+	is.Equal(wrapper.Unwrap(), originalError)
+}
+
+func TestUnwrap(t *testing.T) {
+	is := is.New(t)
+	var originalError = errors.New("something bad happened")
+
+	wrapper := &erkjson.JSONWrapper{}
+	wrapper.SetOriginalError(originalError)
+	is.Equal(errors.Unwrap(wrapper), originalError)
+}
+
+func TestIsNil(t *testing.T) {
+	t.Run("with nil JSONWrapper", func(t *testing.T) {
+		is := is.New(t)
+
+		var wrapper *erkjson.JSONWrapper
+		is.True(wrapper.IsNil())
+	})
+
+	t.Run("with non nil JSONWrapper", func(t *testing.T) {
+		is := is.New(t)
+
+		wrapper := &erkjson.JSONWrapper{}
+		is.Equal(wrapper.IsNil(), false)
+	})
+}
+
+func TestExportError(t *testing.T) {
+	type Entry struct {
+		Name                string
+		Error               error
+		ExpectedErrorString string
+		TypeCheck           func(is *is.I, entry *Entry, exportedError error)
+	}
+
+	table := []Entry{
+		{
+			Name:                "with pointer",
+			Error:               erk.New(&TestPtrWrapableKind{}, "my message"),
+			ExpectedErrorString: `{"kind":"test_ptr_wrapable_kind","message":"my message"}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*TestPtrWrapableKind)
+				is.True(ok)
+			},
+		},
+		{
+			Name:                "with pointer but not wrapable",
+			Error:               erk.New(&TestPtrNonWrapableKind{}, "my message"),
+			ExpectedErrorString: `{"kind":"test_ptr_non_wrapable_kind","message":"my message"}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*erkjson.JSONWrapper)
+				is.True(ok)
+			},
+		},
+		{
+			Name:                "with value",
+			Error:               erk.New(TestValueWrapableKind{}, "my message"),
+			ExpectedErrorString: `{"kind":"test_value_wrapable_kind","message":"my message"}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*erkjson.JSONWrapper) // Since it's a value, it doesn't satisfy the interface
+				is.True(ok)
+			},
+		},
+		{
+			Name:                "with value embedding nil pointer",
+			Error:               erk.New(TestValueWithPtrWrapableKind{}, "my message"),
+			ExpectedErrorString: `{"kind":"test_value_with_ptr_wrapable_kind","message":"my message"}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*erkjson.JSONWrapper) // Since the pointer is nil, it returns the JSONWrapper type
+				is.True(ok)
+			},
+		},
+		{
+			Name:                "with nil kind",
+			Error:               erk.New(nil, "my message"),
+			ExpectedErrorString: `{"kind":"","message":"my message"}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*erkjson.JSONWrapper) // Since the kind is nil, it returns the JSONWrapper type
+				is.True(ok)
+			},
+		},
+		{
+			Name:  "with unmarshalable error",
+			Error: erk.NewWith(&TestPtrWrapableKind{}, "my message", erk.Params{"invalid": make(chan struct{})}),
+			ExpectedErrorString: `{"kind":"erk:error_is_invalid_json",` +
+				`"message":"The error cannot be wrapped as JSON: json: error calling MarshalJSON for type *erk.Error: ` +
+				`json: error calling MarshalJSON for type erk.Params: json: unsupported type: chan struct {}"` +
+				`,"params":{"err":"my message"}}`,
+			TypeCheck: func(is *is.I, entry *Entry, exportedError error) {
+				_, ok := exportedError.(*TestPtrWrapableKind)
+				is.True(ok)
+			},
+		},
+	}
+
+	for _, entry := range table {
+		entry := entry // Pin range variable
+
+		t.Run(entry.Name, func(t *testing.T) {
+			is := is.New(t)
+
+			exportedError := erkjson.ExportError(entry.Error)
+			is.Equal(exportedError.Error(), entry.ExpectedErrorString)
+			entry.TypeCheck(is, &entry, exportedError)
+
+			errWithParams := erk.WithParams(entry.Error, erk.Params{
+				"param1": "value1",
+			})
+			erkjson.ExportError(errWithParams)                         // nolint:errcheck // Original could be modified if not properly cloned
+			is.Equal(exportedError.Error(), entry.ExpectedErrorString) // Ensure original was not modified
+
+			is.Equal(errors.Unwrap(exportedError), entry.Error) // Original error is wrapped
+		})
+	}
+}
+
+type TestValueWithPtrWrapableKind struct {
+	erk.DefaultKind
+	*erkjson.JSONWrapper
+}
+
+func (TestValueWithPtrWrapableKind) KindStringFor(kind erk.Kind) string {
+	return "test_value_with_ptr_wrapable_kind"
+}
+
+type TestValueWrapableKind struct {
+	erk.DefaultKind
+	erkjson.JSONWrapper
+}
+
+func (TestValueWrapableKind) KindStringFor(kind erk.Kind) string {
+	return "test_value_wrapable_kind"
+}
+
+type TestPtrWrapableKind struct {
+	erk.DefaultPtrKind
+	erkjson.JSONWrapper
+}
+
+func (*TestPtrWrapableKind) KindStringFor(kind erk.Kind) string {
+	return "test_ptr_wrapable_kind"
+}
+
+type TestPtrNonWrapableKind struct {
+	erk.DefaultPtrKind
+}
+
+func (*TestPtrNonWrapableKind) KindStringFor(kind erk.Kind) string {
+	return "test_ptr_non_wrapable_kind"
+}
